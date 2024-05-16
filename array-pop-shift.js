428b57c43c7d5b6f2639fa98187622da7b753e94
Théfa <140509878+Thefa07@users.noreply.github.com>
2024-05-16 11:06:24 -0300

    Create array-pop-shift.js

---
+++ array-pop-shift.js
@@ -0,0 +1,10 @@
+var frutas = ['maça', 'Banana', 'Laranja', 'Pêssego', 'Abacaxi'];
+var ultimaFruta = frutas.pop();
+
+console.log('array após remover o último elemento:', frutas);
+console.log('última fruta removida:', ultimaFruta);
+
+var primeiraFruta = frutas.shift();
+
+console.log('array após remover o primeiro elemento;', frutas);
+console.log('primeira fruta removida:', primeiraFruta);
