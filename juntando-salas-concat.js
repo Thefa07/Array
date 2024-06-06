412962679c4514f6fc3dae56ecda017b07124ba0
Théfa <140509878+Thefa07@users.noreply.github.com>
2024-06-06 11:49:12 -0300

    Add files via upload

---
+++ juntando-salas-concat.js
@@ -0,0 +1,5 @@
+const salaJS = ['Ana', 'Beatriz', 'Emilyn', 'Giovanna']
+const salaPython = ['Maria Eduarda', 'Sthefanie', 'Mileni', 'Bernardo']
+
+const salasUnificadas = salaJS.concat(salaPYTHON);
+console.log(salasUnificadas)
