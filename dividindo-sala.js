f395877c09b57d15043fc0b47157e1e696508c10
Théfa <140509878+Thefa07@users.noreply.github.com>
2024-06-06 10:58:57 -0300

    Add files via upload

---
+++ dividindo-sala.js
@@ -0,0 +1,29 @@
+const alunos = [
+    "Dhenifer",
+    "Ana",
+    "Lucas",
+    "Sthefanie",
+    "Beatriz",
+    "Pedro",
+    "Mileni",
+    "Carlos",
+    "Evilyn",
+    "João",
+    "Maria",
+    "Mileni",
+    "Julia",
+    "Thomas",
+    "Pituxa",
+    "Gustavo",
+    "Igor",
+    "Samantha",
+    "Alison",
+    "Giovana",
+    ];
+   
+   const sala1 = alunos.slice(0, 10);
+   const sala2 = alunos.slice(10);
+   
+   
+   console.log(sala1);
+   console.log(sala2);
